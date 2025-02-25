6e78553d4defacfe77bf499de1b91de5a4449924
kasif16 <kasifkhan1611@gmail.com>
2025-02-25 15:35:27 +0530

    Time: 5 ms (77.02%), Space: 12.5 MB (17.93%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -9,11 +9,9 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-         if (node == nullptr || node->next == nullptr) return;
-    swap(node->val, node->next->val);
-    ListNode* temp = node->next;
-    node->next = node->next->next;
-    delete temp;
-        
+        ListNode* temp = node->next; 
+        node->val = temp->val;   
+        node->next = temp->next;      
+        delete temp;                  
     }
 };
